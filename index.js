47b7987071e88517f1a582969e688ed76d4739e5
Aigar Voog <aigar.voog@outlook.com>
2023-11-17 21:19:50 +0200

    part3 - phonebook backend - exc 3.11 done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const app = express();
 app.use(express.json());
 app.use(cors())
+app.use(express.static('dist'))
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan('tiny'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
