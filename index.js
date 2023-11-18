9dcaecea520d9808455ad89c03ffaa2c95cad08b
Aigar Voog <aigar.voog@outlook.com>
2023-11-18 22:11:23 +0200

    part3 - phonebook backend - exc 3.13-3.14 done

--- index.js
+++ index.js
@@ -1,36 +1,28 @@
+require("dotenv").config();
 const express = require("express");
-const morgan = require('morgan')
-const cors = require('cors')
+const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
-app.use(express.json());
-app.use(cors())
-app.use(express.static('dist'))
-morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
-app.use(morgan('tiny'))
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons =[
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
+const Person = require("./models/person");
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static("dist"));
+
+morgan.token("body", function (req, res) {
+  return JSON.stringify(req.body);
+});
+app.use(morgan("tiny"));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+let persons = [];
 
 app.get("/info", (request, response) => {
   response.send(`
@@ -40,7 +32,9 @@ app.get("/info", (request, response) => {
 });
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((people) => {
+    response.json(people);
+  });
 });
 
 app.get("/api/persons/:id", (request, response) => {
@@ -61,10 +55,6 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end();
 });
 
-const generateId = () => {
-  return Math.floor(Math.random() * 999999999);
-};
-
 app.post("/api/persons", (request, response) => {
   const body = request.body;
 
@@ -84,17 +74,18 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: generateId(),
-  };
+  });
 
-  persons = persons.concat(person);
-
-  response.json(person);
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
 });
 
-const PORT = process.env.PORT || 3001
+app.use(unknownEndpoint);
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
